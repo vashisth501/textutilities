706ed54020fd490588506fb2b912d16782711235
vashisth501 <73217475+vashisth501@users.noreply.github.com>
2021-10-03 00:47:06 +0530

    added more functionality for text transform

--- src/Components/Textform.js
+++ src/Components/Textform.js
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Textform(props) {
+    const changeToUpper = ()=>{
+        var uppercaseText = text.toUpperCase();
+        setstate(uppercaseText);    
+    }
+
+    const handlingChange = (event)=>{
+        setstate(event.target.value);
+    }
+
+    const changeToLower = ()=>{
+        var lowerCaseText = text.toLowerCase();
+        setstate(lowerCaseText);
+    }
+    const [text, setstate] = useState("Enter your Text Here")
     return (
-        <div>
+        <>
+        <div className="container">
             <div className="container p-3">
                 <h1>{props.heading}</h1>
             <div className="mb-3">
-            <textarea className="form-control" id="mytextbox" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handlingChange} id="mytextbox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary">Convert to Uppercase</button>
+            <button className="btn btn-primary  mx-1" onClick={changeToUpper}>Convert to UpperCase</button>
+            <button className="btn btn-primary  mx-1" onClick={changeToLower}>Convert to LowerCase</button>
             </div>
         </div>
+        <div className="container">
+            <h1>Your Text Summary</h1>
+            <p>{text.split(" ").length} words and {text.length} alphabets</p>
+            <p>{0.008 * text.split(" ").length} minutes to read</p>
+            <h2 className="mt-2">Preview the Entered Text</h2>
+            <p className="border border-info p-3">{text}</p>
+        </div>
+        </>
     )
 }
 
